485a682a4fb80666f64e87bc47b811e11ec6f354
Marcos Castilho <20149083@tafe.wa.edu.au>
2026-09-18 02:26:36 +0800

    initialize the supplied Firebase data source without tracking configuration

---
+++ lib/services/remote_api_data_source.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:todo_app_marcos/firebase_options.dart';
+import 'package:todo_app_marcos/models/todo.dart';
+import 'package:todo_app_marcos/services/i_data_source.dart';
+
+class RemoteAPIDataSource implements IDataSource {
+  late FirebaseDatabase database;
+
+  Future<void> initialise() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    database = FirebaseDatabase.instance;
+  }
+
+  static Future<RemoteAPIDataSource> createAsync() async {
+    final dataSource = RemoteAPIDataSource();
+    await dataSource.initialise();
+    return dataSource;
+  }
+
+  @override
+  Future<bool> add(Todo model) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Todo>> browse() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> delete(Todo model) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> edit(Todo model) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Todo?> read(String id) {
+    throw UnimplementedError();
+  }
+}
